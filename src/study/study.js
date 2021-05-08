64ec03c530276c6d99d617205039832c070383a1
yifeng <478297605@qq.com>
2021-05-08 18:00:29 +0800

    feat: 5月8 记

--- src/study/study.js
+++ src/study/study.js
@@ -12,24 +12,34 @@ window.addEventListener('load', () => {
     {
       date: '2021/4/28',
       value: [`踩坑：空文件石不能转换成base64`],
-      diary: '今天一天啥也没干，就改这个小bug，结果弄了一天，裂开了呀',
+      diary: '今天一天啥也没干，就改这个小bug，结果弄了一天，裂开了',
     },
     {
       date: '2021/4/29',
-      value: [`使用webpack-cdn-plugin对依赖使用cdn引入`],
+      value: [`webpack-cdn-plugin对依赖使用cdn引入`],
       diary: '改良了一下打卡页面数据渲染的结构',
     },
     {
       date: '2021/4/30',
       value: [
-        `使用自定义属性实现图片懒加载`,
-        `了解Vue.API: $options(可调用自定义属性)，$parent(父实例)，$root(根实例)，$watch(类似watch，可关闭watch)，v-is(特定元素时调用组件)`,
+        `自定义属性实现图片懒加载`,
+        `Vue.API: $options(可调用自定义属性)，$parent(父实例)，$root(根实例)，$watch(类似watch，可关闭watch)，v-is(特定元素时调用组件)`,
       ],
-      diary: '五一了，终于可以打游戏了',
+      diary: '五一了，冲',
     },
     {
       date: '2021/5/6',
-      value: [`熟悉art-template`],
+      value: [`art-template`],
+    },
+    {
+      date: '2021/5/7',
+      value: [`技术调研收银app的双屏异显`],
+      diary: '搞了一下午，如果要做的话需要使用原生安卓，有点难',
+    },
+    {
+      date: '2021/5/8',
+      value: [`fetch`,`vue3 hooks`],
+      diary: '尤雨溪:“读书人的事儿，怎么能叫抄呢”  hooks真香',
     },
   ]
 
