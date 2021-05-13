bb29394dc130f72e4f1e3ec13fcaf4e1a20565ec
yifeng <478297605@qq.com>
2021-05-13 18:21:45 +0800

    feat: 5月13记

--- src/views/study/study.js
+++ src/views/study/study.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
     {
       date: '2021/4/28',
       value: [`踩坑：空文件石不能转换成base64`],
-      diary: '今天一天啥也没干，就改这个小bug，结果弄了一天，裂开了',
+      diary: '今天一天啥也没干，就改这个小bug，弄了一天，裂开了',
     },
     {
       date: '2021/4/29',
@@ -25,11 +25,12 @@ window.addEventListener('load', () => {
         `自定义属性实现图片懒加载`,
         `Vue.API: $options(可调用自定义属性)，$parent(父实例)，$root(根实例)，$watch(类似watch，可关闭watch)，v-is(特定元素时调用组件)`,
       ],
-      diary: '五一了，冲',
+      diary: '五一了',
     },
     {
       date: '2021/5/6',
       value: [`art-template`],
+      diary: '',
     },
     {
       date: '2021/5/7',
@@ -39,11 +40,17 @@ window.addEventListener('load', () => {
     {
       date: '2021/5/8',
       value: [`fetch`, `vue3 hooks`],
-      diary: '尤雨溪:“读书人的事儿，怎么能叫抄呢”  hooks真香',
+      diary: '尤雨溪:“读书人的事儿，怎么能叫抄呢”',
     },
     {
       date: '2021/5/11',
       value: [`vue3 Suspense`],
+      diary: '',
+    },
+    {
+      date: '2021/5/13',
+      value: [`中途岛接口调用和调试`],
+      diary: '写css是个体力活',
     },
   ]
 
